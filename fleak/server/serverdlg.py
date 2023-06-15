4c0b450f2bc3d658b30f256118c6c003eaf81871
zhy <hangyu.zhu@jiangnan.edu.cn>
2023-06-15 14:49:09 +0800

    debug_fedavg

--- fleak/server/serverdlg.py
+++ fleak/server/serverdlg.py
@@ -35,7 +35,7 @@ class Serverdlg(Server):
         grads = OrderedDict()
         for (key, value) in weights.items():
             grads[key] = o_weights[key] - weights[key]
-            grads[key].requires_grad = False
+            # grads[key].requires_grad = False
         return grads
 
     def train_eval(self, clients=None, set_to_use='test'):
@@ -54,7 +54,7 @@ class Serverdlg(Server):
             # convert to gradients
             grads = self.comp_grads(update)
             # update client round
-            self.updates.append((c_id, num_samples, grads))
+            self.updates.append((c_id, num_samples, update, grads))
         eval_accuracy = eval_correct / eval_total
         print('Round %d: ' % self.cur_round + set_to_use + ' accuracy %.4f' % eval_accuracy)
         # update communication round
@@ -79,19 +79,19 @@ class Serverdlg(Server):
             reconstruct_data, reconstruct_label = reconstruct_dlg(self.updates[0][-1], self.dummy_data, self.dummy_labels, self.global_model, 20, 1.0)
         return reconstruct_data, reconstruct_label
 
-    def federated_averaging(self):
-        total_samples = np.sum([update[1] for update in self.updates])
-        averaged_soln = copy.deepcopy(self.updates[0][2])
-        for key in self.global_model.state_dict().keys():
-            if 'num_batches_tracked' in key:
-                continue
-            for i in range(len(self.updates)):
-                if i == 0:
-                    averaged_soln[key] = averaged_soln[key] * self.updates[i][1] / total_samples
-                else:
-                    averaged_soln[key] += self.updates[i][2][key] * self.updates[i][1] / total_samples
-        self.accumulate_momentum(averaged_soln)
-        # update global model
-        self.global_model.load_state_dict(averaged_soln)
-        # clear uploads buffer
-        self.updates = []
+    # def federated_averaging(self):
+    #     total_samples = np.sum([update[1] for update in self.updates])
+    #     averaged_soln = copy.deepcopy(self.updates[0][2])
+    #     for key in self.global_model.state_dict().keys():
+    #         if 'num_batches_tracked' in key:
+    #             continue
+    #         for i in range(len(self.updates)):
+    #             if i == 0:
+    #                 averaged_soln[key] = averaged_soln[key] * self.updates[i][1] / total_samples
+    #             else:
+    #                 averaged_soln[key] += self.updates[i][2][key] * self.updates[i][1] / total_samples
+    #     self.accumulate_momentum(averaged_soln)
+    #     # update global model
+    #     self.global_model.load_state_dict(averaged_soln)
+    #     # clear uploads buffer
+    #     self.updates = []
