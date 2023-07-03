ebc285c05d0257af50955d60d92ba146aa3d565b
xianyu096 <2921569264@qq.com>
2023-07-03 20:05:50 +0800

    idlg
    iDLG

--- fleak/attack/idlg.py
+++ fleak/attack/idlg.py
@@ -2,6 +2,7 @@ import os
 # os.chdir("../..")
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 import copy
 
 device = "cuda" if torch.cuda.is_available() else "CPU"
@@ -9,9 +10,10 @@ device = "cuda" if torch.cuda.is_available() else "CPU"
 def criterion(pred,target):
     return torch.mean(torch.sum(- target * F.log_softmax(pred, dim=-1), 1))
 
-def reconstruct_dlg(shared_gradients, dummy_data, dummy_label, model, epochs=300, lr=0.1):
 
-    optimizer = torch.optim.LBFGS([dummy_data, dummy_label], lr=0.075)
+def reconstruct_dlg(shared_gradients, dummy_data, dummy_label, model, epochs=300, lr=0.075):
+
+    optimizer = torch.optim.LBFGS([dummy_data, dummy_label], lr=lr)
     # gradient closure
     minimal_value_so_far = torch.as_tensor(float("inf"), device="cuda" if torch.cuda.is_available() else "CPU", dtype=torch.float32)
     for iters in range(epochs):
@@ -40,36 +42,35 @@ def reconstruct_dlg(shared_gradients, dummy_data, dummy_label, model, epochs=300
 
         if iters % 10 == 0:
             current_loss = closure()
-            print(print(iters, "%.4f" % current_loss.item()))
+            print(iters, "%.4f" % current_loss.item())
 
 
     return dummy_data,dummy_label
 
 
-def reconstruct_idlg(shared_gradients, data, label, model, epochs=200, lr=1.0):
+def reconstruct_idlg(shared_gradients, dummy_data, dummy_label, model, epochs=200, lr=0.075):
     origin_gradients = shared_gradients.copy()
-    dummy_data = data
-    dummy_label = label
     optimizer = torch.optim.LBFGS([dummy_data, ], lr=lr)
-    loss = []
-
+    # label_pred = torch.argmin(torch.sum(shared_gradients, dim=-1), dim=-1).detach().reshape((1,)).requires_grad(False)
+    ##  use the gradients of FC1 to recover the ground-truth label(but has an error: TypeError: 'bool' object is not callable)
+    label_pred = torch.argmin(torch.sum(list(origin_gradients.values())[-4])).detach().reshape((1,))
+    criterion_idlg = nn.CrossEntropyLoss().to(device)
     for iters in range(epochs):
         def closure():
             optimizer.zero_grad()
             dummy_pred = model(dummy_data)
-            dummy_label_onehot = F.softmax(dummy_label, dim=-1)
-            dummy_loss = criterion(dummy_pred)
+            dummy_loss = criterion_idlg(dummy_pred, label_pred)
             dummy_dy_dx = torch.autograd.grad(dummy_loss, model.parameters(), create_graph=True)
-
             grad_diff = 0
-            for dummy_g, origin_g in zip(dummy_dy_dx, origin_gradients):
+            for dummy_g, origin_g in zip(dummy_dy_dx, shared_gradients.values()):
                 grad_diff += ((dummy_g - origin_g) ** 2).sum()
             grad_diff.backward()
+            return grad_diff
 
         optimizer.step(closure)
 
         if iters % 10 == 0:
             current_loss = closure()
-            loss.append(current_loss)
+            print(iters, "%.4f" % current_loss.item())
 
-    return dummy_data, dummy_label, loss
+    return dummy_data, dummy_label
