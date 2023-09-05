5db5a11befdc76466a6fc292d5a3d6f825d48013
xianyu096 <2921569264@qq.com>
2023-09-05 18:31:22 +0800

    Update GAN.py

--- fleak/attack/GAN.py
+++ fleak/attack/GAN.py
@@ -26,7 +26,7 @@ num_to_merge = 500
 # num_to_merge = 50
 seed = torch.random.normal([num_examples_to_generate, noise_dim])
 seed_merge = torch.random.normal([num_to_merge, noise_dim])
-
+criterion = torch.nn.CrossEntropyLoss().to(device)
 
 class Generator(nn.Module):
     def __init__(self):
@@ -44,72 +44,73 @@ class Generator(nn.Module):
         x = x.view(-1, 32 * 32)
         return x
 
+def discriminator_loss(real_output, fake_output, real_labels):
+    real_loss = criterion(real_labels, real_output)
+
+    fake_result = np.zeros(len(fake_output))
+    # Attack label
+    for i in range(len(fake_result)):
+        fake_result[i] = 10
+    fake_loss = criterion(fake_result, fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+    # Loss of generator
+def generator_loss(fake_output):
+    ideal_result = np.zeros(len(fake_output))
+    # Attack label
+    for i in range(len(ideal_result)):
+        # The class which attacker intends to get
+        ideal_result[i] = 4
+
+    return criterion(ideal_result, fake_output)
+
+def train(dataset, labels, epochs, img_size,generator,discriminator):
+    d_optim = torch.optim.Adam(discriminator.parameters(), lr=0.0001)
+    g_optim = torch.optim.Adam(generator.parameters(), lr=0.001)
+    for epoch in range(epochs):
+        start_time = time.time()
+        for i in range(round(len(dataset) / BATCH_SIZE)):
+            image_batch = dataset[i * BATCH_SIZE:min(len(dataset), (i + 1) * BATCH_SIZE)]
+            labels_batch = labels[i * BATCH_SIZE:min(len(dataset), (i + 1) * BATCH_SIZE)]
+
+            random_noise = torch.randn(img_size, 100, device=device)
+            generated_image = generator(random_noise, traning=True)
+            real_output = discriminator(image_batch, training=False)
+            fake_output = discriminator(generated_image, training=False)
+            gen_loss = generator_loss(fake_output)
+            dis_loss = discriminator_loss(real_output, fake_output, real_label=labels_batch)
+
+            dis_loss.backward()
+            d_optim.step()
+
+            gen_loss.backward()
+            g_optim.step()
+
+        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start_time))
+
+        # Last epoch generate the images and merge them to the dataset
+        generate_and_save(generator, epochs, seed)
+
+def generate_and_save(model, epoch, test_input):
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(6, 6))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(6, 6, i + 1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
 
 def GAN_attack(discriminator, num_classes):
     discriminator.add_module("add_LeakyReLU", nn.LeakyReLU(num_classes, 1))
     generator = Generator().to(device)
-    criterion = torch.nn.CrossEntropyLoss().to(device)
-    d_optim = torch.optim.Adam(discriminator.parameters(), lr=0.0001)
-    g_optim = torch.optim.Adam(generator.parameters(), lr=0.001)
+
 
     # Loss of discriminator
-    def discriminator_loss(real_output, fake_output, real_labels):
-        real_loss = criterion(real_labels, real_output)
 
-        fake_result = np.zeros(len(fake_output))
-        # Attack label
-        for i in range(len(fake_result)):
-            fake_result[i] = 10
-        fake_loss = criterion(fake_result, fake_output)
-        total_loss = real_loss + fake_loss
-        return total_loss
-
-    # Loss of generator
-    def generator_loss(fake_output):
-        ideal_result = np.zeros(len(fake_output))
-        # Attack label
-        for i in range(len(ideal_result)):
-            # The class which attacker intends to get
-            ideal_result[i] = 4
-
-        return criterion(ideal_result, fake_output)
-
-    def train(dataset, labels, epochs, img_size):
-        for epoch in range(epochs):
-            start_time = time.time()
-            for i in range(round(len(dataset) / BATCH_SIZE)):
-                image_batch = dataset[i * BATCH_SIZE:min(len(dataset), (i + 1) * BATCH_SIZE)]
-                labels_batch = labels[i * BATCH_SIZE:min(len(dataset), (i + 1) * BATCH_SIZE)]
-
-                random_noise = torch.randn(img_size, 100, device=device)
-                generated_image = generator(random_noise, traning=True)
-                real_output = discriminator(image_batch, training=False)
-                fake_output = discriminator(generated_image, training=False)
-                gen_loss = generator_loss(fake_output)
-                dis_loss = discriminator_loss(real_output, fake_output, real_label=labels_batch)
-
-                dis_loss.backward()
-                d_optim.step()
-
-                gen_loss.backward()
-                g_optim.step()
-
-            print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start_time))
-
-            # Last epoch generate the images and merge them to the dataset
-            generate_and_save(generator, epochs, seed)
-
-    def generate_and_save(model, epoch, test_input):
-        predictions = model(test_input, training=False)
-
-        fig = plt.figure(figsize=(6, 6))
-
-        for i in range(predictions.shape[0]):
-            plt.subplot(6, 6, i + 1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-            plt.axis('off')
-
-        plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
 
     ## attack
     for i in range(20):
