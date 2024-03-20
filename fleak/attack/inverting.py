191115518167faea089d1b4bcf3b8a3de07c6e5f
xianyu096 <2921569264@qq.com>
2024-03-20 16:33:00 +0800

    Update inverting.py
    inverting-gradient(weight update)

--- fleak/attack/inverting.py
+++ fleak/attack/inverting.py
@@ -1,5 +1,5 @@
 import torch
-from .inverting_class import GradientReconstructor
+from .inverting_class import GradientReconstructor,FedAvgReconstructor
 
 
 def ig(local_model, local_gradients, device="cpu"):
@@ -36,4 +36,29 @@ def ig(local_model, local_gradients, device="cpu"):
                   scoring_choice='loss')
     rec_machine = GradientReconstructor(local_model, (dm, ds), config, num_images=1, device=device)
     output, stats = rec_machine.reconstruct(local_gradients, label_pred)
+    return output, label_pred
+
+
+
+def ig_weight(local_model, local_gradients, device="cpu"):
+    dm = torch.as_tensor([0.4914, 0.4822, 0.4465], device=device, dtype=torch.float32)[None, :, None, None]
+    ds = torch.as_tensor([0.2023, 0.1994, 0.2010], device=device, dtype=torch.float32)[None, :, None, None]
+    label_pred = torch.argmin(torch.sum(list(local_gradients.values())[-2])).detach().reshape((1,))
+    local_model.zero_grad()
+    config = dict(signed=True,
+                  boxed=True,
+                  cost_fn='sim',
+                  indices='def',
+                  weights='equal',
+                  lr=0.1,
+                  optim='adam',
+                  restarts=1,
+                  max_iterations=1000,
+                  total_variation=1e-6,
+                  init='randn',
+                  filter='none',
+                  lr_decay=True,
+                  scoring_choice='loss')
+    rec_machine = FedAvgReconstructor(local_model, (dm, ds), local_steps=5, local_lr=1e-4, config=config, use_updates=True,device=device)
+    output, stats = rec_machine.reconstruct(local_gradients, label_pred, img_shape=(3, 32, 32))
     return output, label_pred
